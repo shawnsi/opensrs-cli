7487eaa223ae6b609b4dde6a4c35d6590d63d21c
Shawn Siefkas <shawn.siefkas@meredith.com>
2012-05-16 11:05:04 -0500

    Updating package version

--- gen-bootstrap.py
+++ gen-bootstrap.py
@@ -3,8 +3,7 @@ output = virtualenv.create_bootstrap_script(textwrap.dedent("""
     import os, subprocess
     def after_install(options, home_dir):
         subprocess.call([join(home_dir, 'bin', 'pip'), 
-            'install', '-e',
-            'git+https://github.com/shawnsi/OpenSRS-py.git#egg=OpenSRS-py'])
+            'install', 'OpenSRS'])
         subprocess.call([join(home_dir, 'bin', 'pip'), 
             'install', 'pyCLI'])
         subprocess.call([join(home_dir, 'bin', 'pip'), 
