3376d25980caa028742f5217d0722e39e41c9ebb
Shawn Siefkas <shawn.siefkas@meredith.com>
2012-07-20 11:15:17 -0500

    Added locking, unlocking, and default nameserver commands

--- opensrscli/__init__.py
+++ opensrscli/__init__.py
@@ -29,7 +29,7 @@ def check_transfer(cli):
 check_transfer.add_argument('domain', nargs='*', help='domains to perform command on')
 
 @CLI
-def nameservers(cli):
+def get_nameservers(cli):
     """
     Returns nameservers for a domain
     """
@@ -50,7 +50,42 @@ def nameservers(cli):
             print '%s: %s' % (domain, response)
         print
 
-nameservers.add_argument('domain', nargs='*', help='domains to perform command on')
+get_nameservers.add_argument('domain', nargs='*', help='domains to perform command on')
+
+@CLI
+def set_nameservers(cli):
+    for domain in cli.args.domain:
+        attrs = {
+            'domain': domain,
+            'type': 'nameservers',
+        }
+        response = cli.opensrs.post("get", "domain", attrs)
+        try:
+            nameservers_dict_list = response['attributes']['nameserver_list']
+            nameserver_list = map(
+                lambda d: d['name'],
+                nameservers_dict_list
+            )
+            attrs = {
+                'domain': domain,
+                'op_type': 'add_remove',
+                'remove_ns': nameserver_list,
+                'add_ns': [
+                    'PDNS1.ULTRADNS.NET',
+                    'PDNS2.ULTRADNS.NET',
+                    'PDNS3.ULTRADNS.ORG',
+                    'PDNS4.ULTRADNS.ORG',
+                    'PDNS5.ULTRADNS.INFO',
+                    'PDNS6.ULTRADNS.CO.UK',
+                ],
+            }
+            response = cli.opensrs.post('advanced_update_nameservers', 'domain', attrs)
+            print '%s: %s' % (domain, response['response_text'])
+        except KeyError:
+            print '%s: %s' % (domain, response)
+        print
+
+set_nameservers.add_argument('domain', nargs='*', help='domains to perform command on')
 
 @CLI
 def transfer(cli):
@@ -68,5 +103,39 @@ def transfer(cli):
         print
 
 transfer.add_argument('domain', nargs='*', help='domains to perform command on')
+
+@CLI
+def lock(cli):
+    """
+    Locks a domain
+    """
+    for domain in cli.args.domain:
+        attrs = {
+            'affect_domains': 0,
+            'domain_name': domain,
+            'data': 'status',
+            'lock_state': 1,
+        }
+        response = cli.opensrs.post('modify', 'domain', attrs)
+        print '%s: %s' % (domain, response['response_text'])
+
+lock.add_argument('domain', nargs='*', help='domains to perform command on')
+
+@CLI
+def unlock(cli):
+    """
+    Unlocks a domain
+    """
+    for domain in cli.args.domain:
+        attrs = {
+            'affect_domains': 0,
+            'domain_name': domain,
+            'data': 'status',
+            'lock_state': 0,
+        }
+        response = cli.opensrs.post('modify', 'domain', attrs)
+        print '%s: %s' % (domain, response['response_text'])
+
+unlock.add_argument('domain', nargs='*', help='domains to perform command on')
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
